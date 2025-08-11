5457ba5c5ffb5a56ee31a8abbd609ab45a1e4eb9
CCOviedo <C.C.Oviedo@hotmail.com>
2025-08-11 12:27:56 +0200

    MapTerrain_v3.6 y Tormenta arena para limite mapa

---
+++ Assets/Scripts/SandStorm.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class SandStorm : MonoBehaviour
+{
+    [SerializeField] Transform teleportPosition; // El prefab SandStorm tiene una position para teletransportar al player
+    [SerializeField] GameObject player;
+}
