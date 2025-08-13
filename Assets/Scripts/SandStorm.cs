1c1f1e1ce8137f53d6879a3784c98ad4c4fc3175
CCOviedo <C.C.Oviedo@hotmail.com>
2025-08-13 09:49:36 +0200

    Tormenta de arena

--- Assets/Scripts/SandStorm.cs
+++ Assets/Scripts/SandStorm.cs
@@ -1,7 +1,72 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
 
 public class SandStorm : MonoBehaviour
 {
     [SerializeField] Transform teleportPosition; // El prefab SandStorm tiene una position para teletransportar al player
-    [SerializeField] GameObject player;
+    //[SerializeField] PlayerController playerController;
+    [SerializeField] TademiusController playerController;
+    [SerializeField] Transform playerPosition;
+    [SerializeField] Volume boxVolume;
+
+    private void Start()
+    {
+        InitalSettings();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    void InitalSettings()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("El player entra en el collider?");
+
+        if (other.CompareTag("Player"))
+        {
+            playerController.playerSpeed /= 2;
+            playerController.runningSpeed /= 2;
+
+            //if (boxVolume != null)
+            //{
+            //    var vig = boxVolume.profile.TryGet(out Vignette vignette);
+            //    Debug.Log("aaaaaaaAAAAAAAa " + vig);
+
+            //    for (int i = 0; i < 100; i++)
+            //    {
+            //        vignette.intensity.value += (i / 100);
+            //    }
+            //}
+
+            StartCoroutine(FaintingPlayer());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerController.playerSpeed *= 2;
+            playerController.runningSpeed *= 2;
+        }
+    }
+
+    IEnumerator FaintingPlayer()
+    {
+        Debug.Log("Antes de irse de la vida");
+        yield return new WaitForSeconds(2f);
+        Debug.Log("El player se desmaya.");
+        Debug.Log("Animacion desmallandose?");
+
+        playerPosition.position = teleportPosition.position;
+
+    }
 }
